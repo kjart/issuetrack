9046e2965ca1cd4061da3ad382f810a5a0f6a390
BIINBELLLO <abdulmateenbinbello@gmail.com>
2019-02-21 13:28:04 +0100

    Added Little Changes

--- main.js
+++ main.js
@@ -10,21 +10,99 @@ function fetchIssues(){
         var severity =issues[i].severity;
         var assignedTo =issues[i].assignedTo;
         var status =issues[i].status;  
+        var replies =issues[i].replies;  
     //put each set of result in an html code
 
-    issuesList.innerHTML += '<div class= "well"'+
+
+                    // Using the status of the issue to know the format of display to use!
+                            if (status=='Closed') {
+
+                                // Checking if there are replies to the issue, so it can also be displayed
+
+                                if (replies==0) {
+    issuesList.innerHTML += '<div class= "row">'+
+                            '<div class= "col-md-6">'+
+                            '<div class= "well">'+
                             '<h6> Issue ID: '+ id+ '</h6>'+
                             '<p><span class = "label label-info">'+ status+ '</span></p>'+
                             '<h3'+ desc+'</h3'+
                             '<p><span class = "glyphicon glyphicon-time"></span>'+ severity+ ' '+
                             '<span class = "glyphicon glyphicon-user"></span>'+ assignedTo + '</p>'+
-                            '<a href = "#" class = "btn btn-warning" onclick = "setStatusClosed(\''+id+'\')">Close</a>'+
-                            '<a href = "#" class = "btn btn-warning" onclick = "deleteIssue(\''+id+'\')">Delete</a>'
+                            '<a href = "#" class = "btn btn-warning" style = "display:none;" id = "close'+id+'" onclick = "setStatusClosed(\''+id+'\')">Close</a>'+
+                            '<a href = "#" class = "btn btn-info"  id = "open'+id+'" onclick = "setStatusOpened(\''+id+'\')">Re-open</a>'+
+                            '<a href = "#" class = "btn btn-success" style = "float:right;" id = "reply'+id+'" onclick = "replyToIssue(\''+id+'\')">Reply</a>'+
 
-                           ' </div>';
+                            '<a href = "#" class = "btn btn-warning" onclick = "deleteIssue(\''+id+'\')">Delete</a>'+
 
-    }//end of for loop
-}
+                            '<br><div class = "well" style = "margin-left:20px;"> <div id = "issueReplies"><p style = "text-align:center; font-weight:bold;">No Replies To This Issue Yet!</p></div></div>'+'</div>'+
+                            
+                            '</div>'+
+                            ' </div>';
+
+                        } //End inner if
+                        else{
+    issuesList.innerHTML += '<div class= "row">'+
+                            '<div class= "col-md-6">'+
+                            '<div class= "well">'+
+                            '<h6> Issue ID: '+ id+ '</h6>'+
+                            '<p><span class = "label label-info">'+ status+ '</span></p>'+
+                            '<h3'+ desc+'</h3'+
+                            '<p><span class = "glyphicon glyphicon-time"></span>'+ severity+ ' '+
+                            '<span class = "glyphicon glyphicon-user"></span>'+ assignedTo + '</p>'+
+                            '<a href = "#" class = "btn btn-warning" style = "display:none;" id = "close'+id+'" onclick = "setStatusClosed(\''+id+'\')">Close</a>'+
+                            '<a href = "#" class = "btn btn-info"  id = "open'+id+'" onclick = "setStatusOpened(\''+id+'\')">Re-open</a>'+
+                            '<a href = "#" class = "btn btn-success" style = "float:right;" id = "reply'+id+'" onclick = "replyToIssue(\''+id+'\')">Reply</a>'+
+
+                            '<a href = "#" class = "btn btn-warning" onclick = "deleteIssue(\''+id+'\')">Delete</a>'+
+
+                            '<br><div class = "well" style = "margin-left:20px;"> <div id = "issueReplies"><p style = "text-align:center; font-weight:bold;">Replies Will Go Here, Am guessing using a loop to fetch the replies from an array!</p></div></div>'+'</div></div>'+' </div>';
+
+
+                            }//End inner else
+                        }//End Outer if
+                            else
+
+                            {
+
+                            if (replies==0) {
+
+                                
+    issuesList.innerHTML += '<div class= "row">'+
+                            '<div class= "col-md-6">'+
+                            '<div class= "well">'+
+                            '<h6> Issue ID: '+ id+ '</h6>'+
+                            '<p><span class = "label label-info">'+ status+ '</span></p>'+
+                            '<h3'+ desc+'</h3'+
+                            '<p><span class = "glyphicon glyphicon-time"></span>'+ severity+ ' '+
+                            '<span class = "glyphicon glyphicon-user"></span>'+ assignedTo + '</p>'+
+                            '<a href = "#" class = "btn btn-warning" id = "close'+id+'" onclick = "setStatusClosed(\''+id+'\')">Close</a>'+
+                            '<a href = "#" class = "btn btn-info" style = "display:none;" id = "open'+id+'" onclick = "setStatusOpened(\''+id+'\')">Re-open</a>'+
+                            '<a href = "#" class = "btn btn-success" style = "float:right;" id = "reply'+id+'" onclick = "replyToIssue(\''+id+'\')">Reply</a>'+
+
+                            '<a href = "#" class = "btn btn-warning" onclick = "deleteIssue(\''+id+'\')">Delete</a>'+
+
+                            '<br><div class = "well" style = "margin-left:20px;"><div id = "issueReplies"><p style = "text-align:center; font-weight:bold;">No Replies To This Issue Yet!</p></div> </div>'+'</div></div> </div>'; 
+                            } else{
+        issuesList.innerHTML += '<div class= "row">'+
+                            '<div class= "col-md-6">'+
+                            '<div class= "well">'+
+                            '<h6> Issue ID: '+ id+ '</h6>'+
+                            '<p><span class = "label label-info">'+ status+ '</span></p>'+
+                            '<h3'+ desc+'</h3'+
+                            '<p><span class = "glyphicon glyphicon-time"></span>'+ severity+ ' '+
+                            '<span class = "glyphicon glyphicon-user"></span>'+ assignedTo + '</p>'+
+                            '<a href = "#" class = "btn btn-warning" id = "close'+id+'" onclick = "setStatusClosed(\''+id+'\')">Close</a>'+
+                            '<a href = "#" class = "btn btn-info" style = "display:none;" id = "open'+id+'" onclick = "setStatusOpened(\''+id+'\')">Re-open</a>'+
+                            '<a href = "#" class = "btn btn-success" style = "float:right;" id = "reply'+id+'" onclick = "replyToIssue(\''+id+'\')">Reply</a>'+
+
+                            '<a href = "#" class = "btn btn-warning" onclick = "deleteIssue(\''+id+'\')">Delete</a>'+
+
+                            '<br><div class = "well" style = "margin-left:20px;"><div id = "issueReplies"><p style = "text-align:center; font-weight:bold;">Replies Will Go Here, Am guessing using a loop to fetch the replies from an array!</p></div> </div>'+'</div></div> </div>';
+                            }                          
+                            }
+
+        }//end of for loop
+    }
 }
     //append an event listener to the form's submit
 
@@ -36,14 +114,15 @@ function fetchIssues(){
         var issueSeverity = document.getElementById('issueSeverityInput').value;
         var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
         var issueStatus = 'open';
+        var reply_no = 0;
         //save the details as object
         var issue = {
             id: issueId,
             description: issueDesc,
             severity: issueSeverity,
             assignedTo: issueAssignedTo,
-            status: issueStatus
-
+            status: issueStatus,
+            replies: reply_no
         }
         //check if the local storage is empty or not before pushing issue in
         if(localStorage.getItem('issues')=== null){
@@ -67,6 +146,21 @@ function fetchIssues(){
         for(var j = 0; j < issues.length;j++){
             if(issues[j].id==id){
                 issues[j].status = "Closed";
+                document.getElementById('open'+id).style.display = 'block';
+                document.getElementById('close'+id).innerHTML = 'Open';
+            }
+        }
+        localStorage.setItem('issues',JSON.stringify(issues)); 
+        fetchIssues();
+    }
+    function setStatusOpened(id){
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        for(var j = 0; j < issues.length;j++){
+            if(issues[j].id==id){
+                issues[j].status = "open";
+                document.getElementById('open'+id).style.display = 'none';
+                document.getElementById('close'+id).style.display = 'block';
+                document.getElementById('close'+id).innerHTML = 'Open';
             }
         }
         localStorage.setItem('issues',JSON.stringify(issues)); 
